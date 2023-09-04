06fdc1598091fe40a61fe6743e15edd528011745
Ellnamin <ellnamin.personal@gmail.com>
2023-09-04 14:45:41 +0330

    notifications app's models created

--- apps/notifications/models.py
+++ apps/notifications/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+cascade = models.CASCADE
+
+class NotificationStatus(models.Model):
+    title = models.CharField(max_length=50)
+
+class Notification(models.Model):
+    recipient = models.ForeignKey(User, cascade)
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    status = models.ForeignKey(NotificationStatus, cascade)
+    
+    def __str__(self):
+        return f'{self.recipient}\t{self.subject}\t{self.status}'
