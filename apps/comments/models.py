34307c843ff1c185a2c72b9f22e6d7b4a7dbddb1
Ellnamin <ellnamin.personal@gmail.com>
2023-09-05 13:48:55 +0330

    Comment model craeted

--- apps/comments/models.py
+++ apps/comments/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+# I created a simple model for comments because it's not the main deal
+class Comment(models.Model):
+    user = models.ForeignKey(User, models.CASCADE)
+    content = models.TextField(max_length=800)
+
+    def __str__(self):
+        return self.content
