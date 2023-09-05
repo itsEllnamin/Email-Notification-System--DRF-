bb5c5e5f4ef5643ff8c77087af3218ff542e26c3
Ellnamin <ellnamin.personal@gmail.com>
2023-09-05 15:22:36 +0330

    create CommentViewSet

--- apps/comments/views.py
+++ apps/comments/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import CommentSerializer
+from .models import Comment
+from notifications.serializers import NotificationSerializer
+from notifications.viewsets import NotificationViewSet
 
-# Create your views here.
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    
+    def perform_create(self, serializer):
+        serializer.save()
+        notification_serializer = NotificationSerializer(data=notification_data)
+        notification_serializer.is_valid(raise_exception=True)
+        notification_view = NotificationViewSet.as_view({"post": "create"})
+        notification_response = notification_view(request=request, serializer=notification_serializer)
